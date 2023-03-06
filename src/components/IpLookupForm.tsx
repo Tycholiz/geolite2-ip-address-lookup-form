c3bdf291629f916420a0f9c16204710349ff38f6
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2023-03-06 15:30:22 -0800

    refactor: pull out the staging part of the IP lookup service to its own component

--- src/components/IpLookupForm.tsx
+++ src/components/IpLookupForm.tsx
@@ -1,48 +1,9 @@
 import { useState } from "react";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
-import styled from "@emotion/styled";
-
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-bottom: 16px;
-`;
-
-const StyledTextField = styled(TextField)`
-  margin-right: 16px;
-`;
-
-const StyledErrorMessage = styled(Typography)`
-  color: red;
-  margin-top: 0;
-`;
+import { StagingIpAddresses } from "./StagingIpAddresses";
 
 export function IpLookupForm() {
-  const [formIpAddress, setFormIpAddress] = useState("");
   const [ipAddresses, setIpAddresses] = useState<string[]>([]);
-  const [isValidIpAddress, setIsValidIpAddress] = useState(true);
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
-    if (ipRegex.test(formIpAddress)) {
-      setIsValidIpAddress(true);
-      setIpAddresses([...ipAddresses, formIpAddress]);
-      setFormIpAddress("");
-    } else {
-      setIsValidIpAddress(false);
-    }
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormIpAddress(event.target.value);
-  };
 
   return (
     <div>
@@ -54,30 +15,10 @@ export function IpLookupForm() {
         To begin, enter an IP address. When you have entered all the IP
         addresses that you want to geolocate, hit the 'Geolocate!' button.
       </Typography>
-      {!isValidIpAddress && (
-        <StyledErrorMessage variant="caption">
-          Please enter a valid IP address.
-        </StyledErrorMessage>
-      )}
-      <StyledForm onSubmit={handleSubmit}>
-        <StyledTextField
-          label="Enter an IP address"
-          variant="outlined"
-          size="small"
-          value={formIpAddress}
-          onChange={handleChange}
-        />
-        <Button type="submit" variant="contained" color="primary">
-          +
-        </Button>
-      </StyledForm>
-      <List>
-        {ipAddresses.map((item, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={item} />
-          </ListItem>
-        ))}
-      </List>
+      <StagingIpAddresses
+        ipAddresses={ipAddresses}
+        setIpAddresses={setIpAddresses}
+      />
     </div>
   );
 }
---
+++ src/components/StagingIpAddresses.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import styled from "@emotion/styled";
+
+type Props = {
+  ipAddresses: string[];
+  setIpAddresses: (arg0: string[]) => void;
+};
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+const StyledTextField = styled(TextField)`
+  margin-right: 16px;
+`;
+
+const StyledErrorMessage = styled(Typography)`
+  color: red;
+  margin-top: 0;
+`;
+
+export function StagingIpAddresses({ ipAddresses, setIpAddresses }: Props) {
+  const [isValidIpAddress, setIsValidIpAddress] = useState(true);
+  const [formIpAddress, setFormIpAddress] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormIpAddress(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+    if (ipRegex.test(formIpAddress)) {
+      setIsValidIpAddress(true);
+      setIpAddresses([...ipAddresses, formIpAddress]);
+      setFormIpAddress("");
+    } else {
+      setIsValidIpAddress(false);
+    }
+  };
+
+  return (
+    <>
+      {!isValidIpAddress && (
+        <StyledErrorMessage variant="caption">
+          Please enter a valid IP address.
+        </StyledErrorMessage>
+      )}
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledTextField
+          label="Enter an IP address"
+          variant="outlined"
+          size="small"
+          value={formIpAddress}
+          onChange={handleChange}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          +
+        </Button>
+      </StyledForm>
+      <List>
+        {ipAddresses.map((item, index) => (
+          <ListItem key={index}>
+            <ListItemText primary={item} />
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
