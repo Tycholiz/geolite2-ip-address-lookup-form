48b77e7a3a7931c3fbc9677f2d9b2316470ac295
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2023-03-07 21:24:16 -0800

    test: add tests for IpLookupForm

---
+++ src/components/__tests__/IpLookupForm.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import { IpLookupForm } from "../IpLookupForm";
+
+global.fetch = jest.fn().mockImplementation(() => ({
+  json: () => Promise.resolve({ result: [] }),
+}));
+
+describe("IpLookupForm", () => {
+  it("adds an IP address to the staging area", () => {
+    const { getByLabelText, getByRole } = render(<IpLookupForm />);
+    const input = getByLabelText("Enter an IP address");
+    const addButton = getByRole("button", { name: "+" });
+
+    fireEvent.change(input, { target: { value: "24.207.47.115" } });
+    fireEvent.click(addButton);
+    expect(getByRole("list")).toHaveTextContent("24.207.47.115");
+  });
+
+  it("fetches IP data and displays results", async () => {
+    const { getByLabelText, getByRole } = render(<IpLookupForm />);
+    const input = getByLabelText("Enter an IP address");
+    const addButton = getByRole("button", { name: "+" });
+    const geolocateButton = getByRole("button", {
+      name: /geolocate/i,
+    });
+
+    fireEvent.change(input, { target: { value: "192.168.0.1" } });
+    fireEvent.click(addButton);
+    fireEvent.click(geolocateButton);
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+  });
+});
