9cd017d5e633e83b04037076649cb897c8c119c6
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2023-03-07 19:32:00 -0800

    fix: disallow local IP addresses in form validation

--- src/components/StagingIpAddresses.tsx
+++ src/components/StagingIpAddresses.tsx
@@ -6,6 +6,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
 import styled from "@emotion/styled";
+import { localIpRegex, ipV4Regex, ipV6Regex } from "../utils/constants";
 
 type Props = {
   ipAddresses: string[];
@@ -38,11 +39,11 @@ export function StagingIpAddresses({ ipAddresses, setIpAddresses }: Props) {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const ipV4Regex = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
-    const ipV6Regex =
-      /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/;
 
-    if (ipV4Regex.test(formIpAddress) || ipV6Regex.test(formIpAddress)) {
+    if (
+      (!localIpRegex.test(formIpAddress) && ipV4Regex.test(formIpAddress)) ||
+      ipV6Regex.test(formIpAddress)
+    ) {
       setIsValidIpAddress(true);
       setIpAddresses([...ipAddresses, formIpAddress]);
       setFormIpAddress("");
